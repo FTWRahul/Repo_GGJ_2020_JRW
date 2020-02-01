fb14eba818668315e34780ec21c91f4c5f1449cf
GitWillG <36644824+GitWillG@users.noreply.github.com>
2020-02-01 18:52:08 -0500

    succ gun
    succ gun succs for 5 seconds, the first 2 seconds its radius expands out to its max range, succs for 3 seconds, then shuts down for a second

---
+++ Proj_GGJ_2020_JRW/Assets/Scripts/Equipment/SuccGun.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SuccGun : NetworkBehaviour, IGunInterface
+{
+
+
+    //Todo 
+    //add animation controller reference
+    public AudioClip fireSound;
+    public float succTime = 0;
+    public float succThreshold = 5f;
+    public bool Reloading = false;
+    public float succLength = 2;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            //not local
+            return;
+        }
+        if (Reloading != true)
+        {
+
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                //charge the laser, then check if its charged
+                succTime += Time.deltaTime;
+                fire();
+            }
+            if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                //if you stop chargining reset charging variable
+                succTime = 0;
+            }
+        }
+
+    }
+    IEnumerator reloadDelay()
+    {
+        yield return new WaitForSeconds(1);
+        Reloading = false;
+        //laserLine.SetPosition(1, transform.position);
+        yield break;
+
+    }
+    public void fire()
+    {
+        if (succTime > succThreshold)
+        {
+            succLength = 2;
+            Reloading = true;
+            reloadDelay();
+            return;
+        }
+        CmdRay((transform.forward.normalized * succLength), (succLength));
+
+    }
+
+
+
+    [Command]
+    public void CmdRay(Vector3 center, float radius)
+    {
+
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius);
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            if (hitColliders[i].CompareTag("Player"){
+                //ToDo apply damage
+            }
+            i++;
+        }
+        if (succTime < 2f)
+        {
+
+            succLength += Time.deltaTime;
+        }
+
+
+
+  
+    }
+}
