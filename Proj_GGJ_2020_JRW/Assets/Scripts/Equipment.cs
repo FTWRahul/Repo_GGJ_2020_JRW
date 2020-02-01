d3e580dc948ed24e44a8081928edcc72495565d4
Jane <glazychevaeo@gmail.com>
2020-02-01 15:24:46 -0500

    Equipment

---
+++ Proj_GGJ_2020_JRW/Assets/Scripts/Equipment.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Type
+{
+    FEET,
+    WEAPON,
+    HEAD,
+}
+public class Equipment : MonoBehaviour
+{
+    public Type type;
+    public int id;
+}
