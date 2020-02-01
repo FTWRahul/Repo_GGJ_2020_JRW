4be58b058d723bc0908d42203c51f8d5a818af20
Jane <glazychevaeo@gmail.com>
2020-02-01 15:18:56 -0500

    Player controller and animation

---
+++ Proj_GGJ_2020_JRW/Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerAnimationController : NetworkBehaviour
+{
+
+    private static readonly int HorizontalF = Animator.StringToHash("Horizontal_f");
+    private static readonly int VerticalF = Animator.StringToHash("Vertical_f");
+    private static readonly int Index = Animator.StringToHash("Index");
+    private static readonly int PickUp = Animator.StringToHash("PickUp");
+    private static readonly int Slap = Animator.StringToHash("Slap");
+    private static readonly int Slapped = Animator.StringToHash("Slapped");
+    private static readonly int HasItem = Animator.StringToHash("HasItem");
+
+    private Animator _animator;
+    
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void UpdateMovement(float horizontal, float vertical)
+    {
+        _animator.SetFloat(HorizontalF, horizontal);
+        _animator.SetFloat(VerticalF, vertical);
+    }
+
+    
+}
---
+++ Proj_GGJ_2020_JRW/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    public float forwardSpeed;
+    public float backwardSpeed;
+    public float jumpForce;
+    public float gravity;
+    public float rayLength;
+
+    private float _verticalSpeed;
+    
+    private CharacterController _characterController;
+    private PlayerAnimationController _playerAnimation;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _playerAnimation = GetComponent<PlayerAnimationController>();
+    }
+
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            Vector3 normalizedDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+            if (normalizedDir.z < 0)
+            {
+                normalizedDir.z *= backwardSpeed;
+            }
+            else
+            {
+                normalizedDir.z *= forwardSpeed;
+            }
+
+            Vector3 dir = normalizedDir * Time.deltaTime;
+            dir = transform.rotation * normalizedDir;
+
+            if (_characterController.isGrounded && Input.GetButtonDown("Jump") && Physics.Raycast(transform.position, Vector3.down, rayLength)) //In case Revert to -Vector3.up//
+            {
+                _verticalSpeed = jumpForce;
+            }
+            else if (!_characterController.isGrounded)
+            {
+                _verticalSpeed -= gravity * Time.deltaTime;
+            }
+
+            dir.y = _verticalSpeed;
+            _characterController.Move(normalizedDir);
+        }
+    }
+}
