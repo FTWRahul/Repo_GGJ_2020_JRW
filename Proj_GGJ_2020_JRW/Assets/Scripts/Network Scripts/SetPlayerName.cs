effdf74224240a489f01ea9622091638e4e6e89b
Jane <glazychevaeo@gmail.com>
2020-02-01 16:54:22 -0500

    Menu networking

--- Proj_GGJ_2020_JRW/Assets/Scripts/Network Scripts/SetPlayerName.cs
+++ Proj_GGJ_2020_JRW/Assets/Scripts/Network Scripts/SetPlayerName.cs
@@ -4,27 +4,26 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class SetPlayerNameScript : MonoBehaviour
+public class SetPlayerName : MonoBehaviour
 {
-    InputField inputField;
+    private TMP_InputField _inputField;
 
     private void Start()
     {
-        inputField = GetComponent<InputField>();
-        inputField.onEndEdit.AddListener(delegate { LockInput(inputField); });
+        _inputField = GetComponent<TMP_InputField>();
+        _inputField.onEndEdit.AddListener(delegate { LockInput(_inputField); });
     }
 
-    void LockInput(InputField input)
+    void LockInput(TMP_InputField input)
     {
         if (input.text.Length > 0)
         {
             Debug.Log("Text has been entered");
-            FindObjectOfType<CustomNetworkManager>().SetPlayerName(inputField.text);
+            FindObjectOfType<CustomNetworkManager>().SetPlayerName(_inputField.text);
         }
         else if (input.text.Length == 0)
         {
             Debug.Log("Main Input Empty");
         }
     }
-
 }
