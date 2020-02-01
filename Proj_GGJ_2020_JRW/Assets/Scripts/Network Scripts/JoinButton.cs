794ea1e2ab9cadfb504ee75a2a6f36be08072d3b
FTWRahul <r.poundarik@gmail.com>
2020-02-01 15:25:12 -0500

    Network scripts

---
+++ Proj_GGJ_2020_JRW/Assets/Scripts/Network Scripts/JoinButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Networking.Match;
+using TMPro;
+using UnityEngine.UI;
+
+public class JoinButton : MonoBehaviour
+{
+    TextMeshProUGUI buttonText;
+    MatchInfoSnapshot match;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        buttonText = GetComponentInChildren<TextMeshProUGUI>();
+        GetComponent<Button>().onClick.AddListener(JoinMatch);
+    }
+
+    internal void initialize(MatchInfoSnapshot match, Transform panelTransform)
+    {
+        this.match = match;
+        buttonText.text = match.name;
+        transform.SetParent(panelTransform);
+        transform.localScale = Vector3.one;
+        transform.localRotation = Quaternion.identity;
+        transform.localPosition = Vector3.zero;
+    }
+
+    private void JoinMatch()
+    {
+        FindObjectOfType<CustomNetworkManager>().JoinMatch(match);
+    }
+}
