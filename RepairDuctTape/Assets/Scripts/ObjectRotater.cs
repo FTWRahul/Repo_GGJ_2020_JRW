8a7d06416a904e7ad9564ecdb95de6640b8804ff
FTWRahul <r.poundarik@gmail.com>
2020-02-02 14:10:00 -0500

    Basic rotation and tape start

---
+++ RepairDuctTape/Assets/Scripts/ObjectRotater.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ObjectRotater : MonoBehaviour
+{
+    public List<GameObject> itemsToSpawn;
+    public float speed;
+
+    private GameObject _currentObj;
+
+    private void Awake()
+    {
+        int rand = Random.Range(0, itemsToSpawn.Count);
+        _currentObj = Instantiate(itemsToSpawn[rand]);
+        _currentObj.transform.parent = this.transform;
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float y = -Input.GetAxis("Horizontal")* speed * Time.deltaTime;
+        
+        RotateObj(x,y);
+    }
+
+    public void RotateObj(float x, float y)
+    {
+        _currentObj.transform.Rotate(x, y, 0, Space.World);
+    }
+}
---
+++ RepairDuctTape/Assets/Scripts/Tape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tape : MonoBehaviour
+{
+    private Vector2 minMaxHeight;
+
+    private void Awake()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Update()
+    {
+        var pos = transform.position;
+        pos.y = Mathf.Clamp(transform.position.y, minMaxHeight.x, minMaxHeight.y);
+        transform.position = pos;
+    }
+}
