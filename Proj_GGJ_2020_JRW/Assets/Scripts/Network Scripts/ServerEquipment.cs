802fdaec95b69e20aa95b42cb0079c64fb8b8450
GitWillG <36644824+GitWillG@users.noreply.github.com>
2020-02-01 23:07:50 -0500

    Server equipment
    server equipment

---
+++ Proj_GGJ_2020_JRW/Assets/Scripts/Network Scripts/ServerEquipment.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerEquipment : MonoBehaviour
+{
+
+    public List<GameObject> PHead;
+    public List<GameObject> PWeapon;
+    public List<GameObject> PFeet;
+
+    public void equip(bodyComponent component, int newID)
+    {
+        switch (component)
+        {
+            case bodyComponent.HEAD:
+                PHead[newID].SetActive(false);
+                break;
+            case bodyComponent.WEAPON:
+                PWeapon[newID].SetActive(false);
+                break;
+            case bodyComponent.FEET:
+                PFeet[newID].SetActive(false);
+                break;
+
+        }
+
+
+    }
+
+    public void unEquip(bodyComponent component, int newID, Transform DropLocation)
+    {
+        switch (component)
+        {
+            case bodyComponent.HEAD:
+                PHead[newID].transform.position = DropLocation.position;
+                PHead[newID].SetActive(true);
+                break;
+            case bodyComponent.WEAPON:
+                PWeapon[newID].transform.position = DropLocation.position;
+                PWeapon[newID].SetActive(true);
+                break;
+            case bodyComponent.FEET:
+                PFeet[newID].transform.position = DropLocation.position;
+                PFeet[newID].SetActive(true);
+                break;
+
+        }
+        gameObject.transform.localPosition = Spawn.position;
+        gameObject.SetActive(true);
+
+    }
+
+}
