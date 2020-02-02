1dd299e61d1f38b40d935c2d9b95250a4955bb33
Jane <glazychevaeo@gmail.com>
2020-02-02 16:44:46 -0500

    Game manager

--- RepairDuctTape/Assets/Scripts/GameManager.cs
+++ RepairDuctTape/Assets/Scripts/GameManager.cs
@@ -11,8 +11,8 @@ public class GameManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void ExitGame()
     {
-        
+        Application.Quit();
     }
 }
