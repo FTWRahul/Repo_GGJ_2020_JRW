b6ac56ea1b415e621e83e8d47c468093c97f6966
FTWRahul <r.poundarik@gmail.com>
2020-02-01 19:15:48 -0500

    Game manager and player spawning started

--- Proj_GGJ_2020_JRW/Assets/Scripts/GameManager.cs
+++ Proj_GGJ_2020_JRW/Assets/Scripts/GameManager.cs
@@ -1,18 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using Random = System.Random;
 
-public class GameManager : MonoBehaviour
+public class GameManager : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance;
+    
+    public List<Transform> playerSpawnPositions;
+
+    private List<PlayerIdentity> _playerList;
+
+    public List<PlayerIdentity> PlayerList
     {
-        
+        get
+        {
+            if (_playerList.Count == 2)
+            {
+                LaunchPlayers();
+            }
+            return _playerList;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    //Spawn the players in the game
+    public void LaunchPlayers()
+    {
+        for (int i = 0; i < _playerList.Count; i++)
+        {
+            int rand = UnityEngine.Random.Range(0, playerSpawnPositions.Count);
+            _playerList[i].CmdSpawnPlayer(playerSpawnPositions[rand].position);
+            playerSpawnPositions.RemoveAt(rand);
+        }
     }
 }
