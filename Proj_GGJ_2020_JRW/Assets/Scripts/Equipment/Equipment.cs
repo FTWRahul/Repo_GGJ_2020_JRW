0321f4b96180f2e3bd0e26d8b9479640fce0d180
GitWillG <36644824+GitWillG@users.noreply.github.com>
2020-02-01 21:37:35 -0500

    equipment manager
    title

---
+++ Proj_GGJ_2020_JRW/Assets/Scripts/Equipment/Equipment.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum bodyComponent
+{
+    FEET,
+    WEAPON,
+    HEAD,
+}
+public class Equipment : MonoBehaviour
+{
+    public bodyComponent type;
+    public int id;
+
+
+}
---
+++ Proj_GGJ_2020_JRW/Assets/Scripts/Equipment/EquipmentManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentManager : MonoBehaviour
+{
+
+    public List<GameObject> Head;
+    public List<GameObject> Weapon;
+    public List<GameObject> Feet;
+    public int currentHead;
+    public int currentWeapon;
+    public int currentFeet;
+
+    void EquipCorrect(bodyComponent component, int newID)
+    {
+        
+       
+        switch (component)
+        {
+            case bodyComponent.HEAD:
+                Head[currentHead].SetActive(false);
+                //drop server side
+                currentHead = newID;
+                Head[newID].SetActive(true);
+                //equip server side
+
+                break;
+            case bodyComponent.WEAPON:
+                Weapon[currentWeapon].SetActive(false);
+                //drop server side
+                currentWeapon = newID;
+                Weapon[newID].SetActive(true);
+                //equip server side
+
+                break;
+            case bodyComponent.FEET:
+                Feet[currentFeet].SetActive(false);
+                //drop server side
+                currentFeet = newID;
+                Feet[newID].SetActive(true);
+                //equip server side
+
+                break;
+
+        }
+
+
+    }
+
+
+}
