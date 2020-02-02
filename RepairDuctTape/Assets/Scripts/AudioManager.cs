9fb382d7f2ab61b0faaab2c0e9c524f91c1dca19
Jane <glazychevaeo@gmail.com>
2020-02-02 16:35:17 -0500

    WE fucked

---
+++ RepairDuctTape/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] tapeSfx;
+    public AudioClip[] maleSfx;
+    public AudioClip completeSfx;
+
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void TapePlay()
+    {
+        PlaySfx(tapeSfx[Random.Range(0, tapeSfx.Length-1)]);
+    }
+
+    public void CompletePlay()
+    {
+        PlaySfx(completeSfx);
+    }
+
+    public void MalePlay()
+    {
+        PlaySfx(tapeSfx[Random.Range(0, maleSfx.Length-1)]);
+    }
+
+    public void StopSfx()
+    {
+        _audioSource.Stop();
+    }
+    
+    public void PlaySfx(AudioClip clip)
+    {
+        StopSfx();
+        _audioSource.clip = clip;
+        _audioSource.Play();
+    }
+}
