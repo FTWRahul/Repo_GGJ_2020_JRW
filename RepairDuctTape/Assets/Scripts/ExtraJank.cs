87060881aa2d3ecee01a4b6d6dddc350dcb000cc
FTWRahul <r.poundarik@gmail.com>
2020-02-02 16:04:11 -0500

    jank

---
+++ RepairDuctTape/Assets/Scripts/ExtraJank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraJank : MonoBehaviour
+{
+    private void Awake()
+    {
+        this.gameObject.isStatic = true;
+    }
+
+    private void OnDestroy()
+    {
+    }
+}
---
+++ RepairDuctTape/Assets/Scripts/Jank.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jank : MonoBehaviour
+{
+    private void Update()
+    {
+        if (transform.childCount > 3)
+        {
+            transform.GetChild(4).transform.parent = ObjectRotater._currentObj.transform;
+        }
+    }
+}
